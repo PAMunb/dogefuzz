60b88b3475669a7081aaa55451b171a0ff347ea9
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-04-25 23:47:46 -0300

    refactor: moved implementation of ga methods

--- fuzz/ga_power_schedule.go
+++ fuzz/ga_power_schedule.go
@@ -1,10 +1,10 @@
 package fuzz
 
 import (
-	"math"
 	"strings"
 
 	"github.com/dogefuzz/dogefuzz/config"
+	ga "github.com/dogefuzz/dogefuzz/fuzz/genetic_operators"
 	"github.com/dogefuzz/dogefuzz/pkg/common"
 	"github.com/dogefuzz/dogefuzz/pkg/interfaces"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -54,22 +54,22 @@ func (s *geneticAlgorithmPowerSchedule) RequestSeeds(functionId string, strategy
 	orderer.OrderTransactions(transactions)
 
 	seeds := make([][]string, 0)
-	// it will take all seeds
+	// it will takes all seeds
 	for idx := 0; idx < len(transactions); idx++ {
 		seeds = append(seeds, transactions[idx].Inputs)
 	}
 
 	// select seeds to crossover
-	var selectedSeeds [][]string
+	selectedSeeds := make([][]string, 0)
 	for range seeds {
-		selectedSeeds = append(selectedSeeds, rouletteWheelSelection(seeds))
+		selectedSeeds = append(selectedSeeds, ga.Selection(seeds))
 	}
 
 	// do crossover
-	var crossoverSeeds [][]string
+	crossoverSeeds := make([][]string, 0)
 	if len(selectedSeeds) > 2 {
-		for i := 0; i < s.cfg.FuzzerConfig.SeedsSize*2; i++ {
-			seed1, seed2 := crossover(chooseSeedsToCrossover(selectedSeeds))
+		for i := 0; i < len(selectedSeeds); i++ {
+			seed1, seed2 := ga.Crossover(chooseSeedsToCrossover(selectedSeeds))
 			crossoverSeeds = append(crossoverSeeds, seed1)
 			crossoverSeeds = append(crossoverSeeds, seed2)
 		}
@@ -138,84 +138,6 @@ func (s *geneticAlgorithmPowerSchedule) deserializeSeedsList(method abi.Method, 
 	return result, nil
 }
 
-func rouletteWheelSelection(seedsList [][]string) []string {
-	rnd := common.RandomFloat64()
-	lengthList := len(seedsList)
-	var seedsSlice [][]string
-
-	if rnd >= 0 && rnd < FIRST_INTERVAL {
-		seedsSlice = seedsList[0:maxIndex(lengthList, FIRST_RANGE)]
-	} else if rnd >= FIRST_INTERVAL && rnd < SECOND_INTERVAL {
-		seedsSlice = seedsList[minIndex(lengthList, FIRST_RANGE):maxIndex(lengthList, SECOND_RANGE)]
-	} else {
-		// [minIndex:len(seedsList)]
-		seedsSlice = seedsList[minIndex(lengthList, SECOND_RANGE):]
-	}
-
-	return common.RandomChoice(seedsSlice)
-}
-
 func chooseSeedsToCrossover(seedsList [][]string) ([]string, []string) {
 	return common.RandomChoice(seedsList), common.RandomChoice(seedsList)
-}
-
-// metodos a serem testados podem ter mais de um parametro de entrada
-// uma seed representa uma lista de paramentros de entrada
-func crossover(seed1, seed2 []string) ([]string, []string) {
-	var crossoverSeed1 []string
-	var crossoverSeed2 []string
-
-	if len(seed1) == len(seed2) {
-		for i := range seed1 {
-			smallest := smallestSize(seed1[i], seed2[i])
-			// analisar a necessidade desta verificacao
-			if smallest == 0 {
-				crossoverSeed1 = seed1
-				crossoverSeed2 = seed2
-				//	funcionando como esperado
-			} else if smallest == 1 {
-				var tempSeed1 string
-				var tempSeed2 string
-				for j := 0; j < smallest; j++ {
-					tempSeed1 = seed1[i][j:j+1] + seed2[i][j:j+1]
-					tempSeed2 = seed2[i][j:j+1] + seed1[i][j:j+1]
-				}
-				crossoverSeed1 = append(crossoverSeed1, tempSeed1)
-				crossoverSeed2 = append(crossoverSeed2, tempSeed2)
-			} else {
-				var tempSeed1 string
-				var tempSeed2 string
-				for j := 0; j < smallest; j++ {
-
-					if j%2 == 0 {
-						tempSeed1 += seed2[i][j : j+1]
-						tempSeed2 += seed1[i][j : j+1]
-					} else {
-						tempSeed1 += seed1[i][j : j+1]
-						tempSeed2 += seed2[i][j : j+1]
-					}
-				}
-				crossoverSeed1 = append(crossoverSeed1, tempSeed1)
-				crossoverSeed2 = append(crossoverSeed2, tempSeed2)
-			}
-		}
-	}
-
-	return crossoverSeed1, crossoverSeed2
-}
-
-func smallestSize(seed1, seed2 string) int {
-	if len(seed1) >= len(seed2) {
-		return len(seed2)
-	}
-
-	return len(seed1)
-}
-
-func maxIndex(lengthList int, factor float64) int {
-	return int(math.Ceil(float64(lengthList) * factor))
-}
-
-func minIndex(lengthList int, factor float64) int {
-	return int(math.Floor(float64(lengthList) * factor))
 }
